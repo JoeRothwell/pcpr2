398f78b01f70b326fbb508ca76d6ec6c163366d1
JoeRothwell <johndoe@example.com>
2019-10-08 18:18:34 +0200

    Added argument checking and list output

--- R/runPCPR2.R
+++ R/runPCPR2.R
@@ -3,11 +3,17 @@
 #' Runs the PC-PR2 method on X, an omics matrix of intensities, and Z, the subject metadata to be assessed in the model.
 #' @param X A matrix of omics data. It is recommended that the data be appropriately scaled and log transformed if necessary.
 #' @param Z The metadata variables, with the same number of observations as X, whose influence on the omics data (X-matrix) is to be assessed. Categorical variables should be coded as factors.
-#' @param pct_threshold The proportion of variability desired to be explained. Defaults to 0.8.
-#' @keywords pcpr2, principal component analysis, pca, omics, metabolomics, transcriptomics
+#' @param pct.threshold The proportion of variability desired to be explained. Defaults to 0.8.
+#' @keywords pcpr2, Rpartial2, principal component analysis, pca, omics, metabolomics, transcriptomics
 #' @examples output <- runPCPR2(transcripts, Z_metadata)
 #' @examples output
 #' @export
+#' @return Returns a list with 5 elements:
+#' \item{dimensions}{dimensions of the -omics matrix}
+#' \item{types}{data types for each Z-variable}
+#' \item{threshold}{the chosen variability to be explained}
+#' \item{pcn}{the number of principal components used for the chosen threshold}
+#' \item{pR2}{Rpartial2 values for each Z-variable and the overall R2}
 runPCPR2 <- function(X, Z, pct.threshold = 0.8) {
 
   # The following lines are not used but have been kept from the original script
@@ -25,8 +31,16 @@ runPCPR2 <- function(X, Z, pct.threshold = 0.8) {
   #X_DataMatrixScaled = scaling(X_DataMatrixCentered, type = "pareto")
 
   # Load test data
-  X_DataMatrixScaled <- X
+  X_DataMatrix <- X
   Z_Meta <- Z
+
+  # Check arguments:
+            # X is a numeric matrix with no NAs
+  stopifnot(is.numeric(X_DataMatrix),
+            is.matrix(X_DataMatrix),
+            !anyNA(X_DataMatrix),
+            # Same number of n for X and metadata
+            nrow(X_DataMatrix) == nrow(Z_Meta))
 
   Z_MetaRowN <- nrow(Z_Meta)
   Z_MetaColN <- ncol(Z_Meta)
@@ -35,15 +49,15 @@ runPCPR2 <- function(X, Z, pct.threshold = 0.8) {
 
   # Obtain eigenvectors
   pct.threshold <- 0.8 # set variability desired to be explained
-  X_DataMatrixScaled_t <- t(X_DataMatrixScaled)
-  symMat <- X_DataMatrixScaled %*% X_DataMatrixScaled_t
+  X_DataMatrix_t <- t(X_DataMatrix)
+  symMat <- X_DataMatrix %*% X_DataMatrix_t
   eigenData    <- eigen(symMat)
   eigenVal     <- eigenData$values
   eigenVecMat  <- eigenData$vectors
   percents_PCs <- eigenVal/sum(eigenVal)
 
   # Get number of PCs required for threshold (force min to 3)
-  my_counter_2 <- sum(1 - cumsum(rev(percents_PCs)) <= 0.8)
+  my_counter_2 <- sum(1 - cumsum(rev(percents_PCs)) <= pct.threshold)
   if(my_counter_2 > 3) pc_n <- my_counter_2 else pc_n <- 3
 
   pc_data_matrix <- eigenVecMat[, 1:pc_n ]
@@ -84,18 +98,26 @@ runPCPR2 <- function(X, Z, pct.threshold = 0.8) {
   colnames(partialR2MatWtProp) <- NULL
   pR2Sums <- colSums(partialR2MatWtProp) * 100
   names(pR2Sums) <- c(ColNames, "R2")
-  return(pR2Sums)
+
+  # Return results
+  output <- list(dimensions = dim(X_DataMatrix),
+                 types = sapply(Z_Meta, class),
+                 threshold = pct.threshold,
+                 pcn = pc_n,
+                 pR2 = pR2Sums)
+  class(output) <- c("pcpr2", "list")
+  return(output)
 }
 
-#' Plot PC-PR2 output (to be deprecated in favour of an S3 class plot method)
+#' Plot PC-PR2 output (to be deprecated)
 #'
-#' A wrapper for barplot() that plots PC-PR2 output.
+#' A wrapper for barplot() that plots PC-PR2 output. To shortly be deprecated in favour of an S3 class plot method.
 #' @param Rpartial2 Named vector of partial R2 values generated from runPCPR2().
 #' @param ... Other arguments passed to barplot().
 #' @examples plotProp(output)
 #' @seealso \code{\link{barplot}} which this function wraps.
 #' @export
-plotProp <- function(Rpartial2, ...) {
+plotProp <- function(output, ...) {
   bp <- barplot(unname(Rpartial2), ylab = "Weighted Rpartial2", ylim = c(0, max(Rpartial2) * 1.3),
                 xlab = "", col = "red", las=2, ...)
   axis(1, at = bp, labels = c(names(Rpartial2)), cex.axis = 0.8, las=2)
